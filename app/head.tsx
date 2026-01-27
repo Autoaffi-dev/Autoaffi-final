a1cc41c7a4724490c8725377212c4a2c1764fcf4
Linse <autooaffii@gmail.com>
2026-01-28 00:01:15 +0100

    Impact verification

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="impact-site-verification"
+        content="d1d1278d-a7dc-4e19-afc6-192ae570c84a"
+      />
+    </>
+  );
+}
